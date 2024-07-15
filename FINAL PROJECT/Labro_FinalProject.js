42ea0a5b0a0381d547418301e6b7ec8148d96c44
JohnlerryLabro14 <154504047+JohnlerryLabro14@users.noreply.github.com>
2024-07-15 19:11:46 +0800

    Update Labro_FinalProject.js

--- FINAL PROJECT/Labro_FinalProject.js
+++ FINAL PROJECT/Labro_FinalProject.js
@@ -3,27 +3,21 @@
 function handleHeaderVisibility() {
     const header = document.querySelector('header');
     let lastScrollTop = 0;
-
     window.addEventListener('scroll', function() {
         let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
         if (scrollTop > lastScrollTop) {
             header.classList.add('hide-header');
         } else {
             header.classList.remove('hide-header');
         }
-
         lastScrollTop = scrollTop;
     });
 }
-
 function smoothScroll() {
     const navLinks = document.querySelectorAll('nav ul li a');
-
     navLinks.forEach(anchor => {
         anchor.addEventListener('click', function(e) {
             e.preventDefault();
-
             const targetId = this.getAttribute('href').substring(1);
             const targetSection = document.getElementById(targetId);
 
